af05696a07289fd7ae53e5e9b5ccdc6c6fe7dfd2
zymelaii <13282135976@sina.cn>
2023-03-17 16:35:06 +0800

    feat: restruct AST defination

--- src/ast.cpp
+++ src/ast.cpp
@@ -1,24 +1,3 @@
 #include "ast.h"
 
-namespace ssyc {
-
-AstNode::AstNode()
-    : flags{}
-    , scope{0}
-    , parent{nullptr}
-    , left{nullptr}
-    , right{nullptr} {}
-
-AstNode::~AstNode() {
-    if (left != nullptr) {
-        delete left;
-        left = nullptr;
-    }
-
-    if (right != nullptr) {
-        delete right;
-        right = nullptr;
-    }
-}
-
-} // namespace ssyc
+namespace ssyc::ast {} // namespace ssyc::ast
--- src/ast.h
+++ src/ast.h
@@ -2,45 +2,273 @@
 
 #include "token.h"  // IWYU pragma: export
 #include "syntax.h" // IWYU pragma: export
-#include <concepts>
 #include <stdint.h>
+#include <concepts>
 #include <string>
 #include <variant>
+#include <optional>
 
-namespace ssyc {
+namespace ssyc::ast {
 
-struct AstNode {
-    AstNode();
-    ~AstNode();
-
-    struct ScopeID {
-        uint32_t depth : 8;  //!< NOTE: 最多支持 256 级作用域
-        uint32_t hash  : 24; //!< 当前作用域的哈希值
-    } scope;
-
-    union {
-        uint32_t flags;
-
-        struct {
-            bool FLAGS_ConstEval;    //!< 编译期可求值
-            bool FLAGS_Constant;     //!< 内容不可变
-            bool FLAGS_EmptyBlock;   //!< 空代码块
-            bool FLAGS_Arithmetical; //!< 可运算的
-            bool FLAGS_ArrayBlock;   //!< 数组元素块
-            bool FLAGS_Nested;       //!< 嵌套语法节点
-        };
-    };
-
-    union {
-        TokenType  token;
-        SyntaxType syntax;
-    };
-
-    std::variant<std::monostate, std::string, int, float> value;
-
-    AstNode *parent;
-
-    AstNode *left, *right;
+struct ProgramUnit {
+    virtual std::string id() const = 0;
 };
 
-} // namespace ssyc
+struct Program;
+struct FuncDef;
+struct Block;
+struct Statement;
+struct DeclStatement;
+struct Expr;
+struct TypeDecl;
+struct InitializeList;
+struct UnaryExpr;
+struct BinaryExpr;
+struct FnCallExpr;
+struct ConstExprExpr;
+struct OrphanExpr;
+
+struct TypeDecl final : public ProgramUnit {
+    std::string id() const override {
+        return "type-decl";
+    }
+
+    enum class Type : uint8_t {
+        Integer,
+        Float,
+    };
+
+    //! 指针型类型
+    bool pointerLike() const {
+        return !optSubscriptList.has_value()
+            && optSubscriptList.value().size() > 0
+            && optSubscriptList.value()[0] == nullptr;
+    }
+
+    Type                               type;             //!< 基本类型
+    bool                               constant;         //!< const 类型
+    std::string                        ident;            //!< 标识符
+    std::optional<std::vector<Expr *>> optSubscriptList; //!< 数组下标列表
+};
+
+struct InitializeList final : public ProgramUnit {
+    std::string id() const override {
+        return "initialize-list";
+    }
+
+    std::vector<Expr *> valueList; //!< 数值列表
+};
+
+struct Expr : public ProgramUnit {
+    virtual std::string id() const       = 0;
+    virtual bool        writable() const = 0;
+
+    enum class Type : uint8_t {
+        Unary,     //!< 一元表达式
+        Binary,    //!< 二元表达式
+        FnCall,    //!< 函数调用
+        ConstExpr, //!< 编译期取值
+        Orphan,    //!< 孤立值
+                //!< NOTE: Orphan 是初始化列表与非 ConstExpr 的变量值
+    };
+
+    Type type; //!< 表达式类型
+};
+
+struct UnaryExpr final : public Expr {
+    std::string id() const override {
+        return "unary-expr";
+    }
+
+    inline bool writable() const override {
+        return false;
+    }
+
+    UnaryExpr() {
+        Expr::type = Expr::Type::Unary;
+    }
+
+    enum class Type : uint8_t {
+        POS,  //!< 取正
+        NEG,  //!< 取负
+        LNOT, //!< 逻辑非
+    };
+
+    Type  op;      //!< 操作符
+    Expr *operand; //!< 操作数
+};
+
+struct BinaryExpr final : public Expr {
+    std::string id() const override {
+        return "binary-expr";
+    }
+
+    inline bool writable() const override {
+        //! FIXME: 多维数组索引的左操作数实际是不一定可写的
+        return lhs->writable() && (op == Type::ASSIGN || op == Type::SUBSCRIPT);
+    }
+
+    BinaryExpr() {
+        Expr::type = Expr::Type::Binary;
+    }
+
+    enum class Type : uint8_t {
+        ASSIGN,    //!< 赋值
+        ADD,       //!< 加法
+        SUB,       //!< 减法
+        MUL,       //!< 乘法
+        DIV,       //!< 除法
+        MOD,       //!< 取模
+        LT,        //!< 小于
+        LE,        //!< 小于等于
+        GT,        //!< 大于
+        GE,        //!< 大于等于
+        EQ,        //!< 等于
+        NE,        //!< 不等于
+        LAND,      //!< 逻辑与
+        LOR,       //!< 逻辑或
+        SUBSCRIPT, //!< 数组索引
+    };
+
+    Type  op;  //!< 操作符
+    Expr *lhs; //!< 左操作数
+    Expr *rhs; //!< 右操作数
+};
+
+struct FnCallExpr final : public Expr {
+    std::string id() const override {
+        return "func-call";
+    }
+
+    inline bool writable() const override {
+        return false;
+    }
+
+    FnCallExpr() {
+        Expr::type = Expr::Type::FnCall;
+    }
+
+    FuncDef            *func;   //!< 函数目标
+    std::vector<Expr *> params; //!< 参数列表
+};
+
+struct ConstExprExpr final : public Expr {
+    std::string id() const override {
+        return "constexpr";
+    }
+
+    inline bool writable() const override {
+        return false;
+    }
+
+    ConstExprExpr() {
+        Expr::type = Expr::Type::ConstExpr;
+    }
+
+    enum class Type : uint8_t {
+        Integer,
+        Float,
+    };
+
+    Type                                         type;  //!< 类型
+    std::variant<std::monostate, int32_t, float> value; //!< 数值
+};
+
+struct OrphanExpr final : public Expr {
+    std::string id() const override {
+        return "orphan-expr";
+    }
+
+    inline bool writable() const override {
+        return std::holds_alternative<TypeDecl *>(ref)
+            && !std::get<TypeDecl *>(ref)->constant;
+    }
+
+    OrphanExpr() {
+        Expr::type = Expr::Type::Orphan;
+    }
+
+    using OrphanType =
+        std::variant<std::monostate, TypeDecl *, InitializeList *>;
+    OrphanType ref; //!< 目标引用
+};
+
+struct Statement : public ProgramUnit {
+    virtual std::string id() const = 0;
+
+    enum class Type : uint8_t {
+        Decl,
+        Expr,
+        IfElse,
+        While,
+        Break,
+        Continue,
+        Return,
+    };
+
+    Type type; //!< 语句类型
+};
+
+struct DeclStatement final : public Statement {
+    std::string id() const override {
+        return "decl-statement";
+    }
+
+    DeclStatement() {
+        Statement::type = Statement::Type::Decl;
+    }
+
+    std::vector<std::pair<TypeDecl *, Expr *>> declList; //!< 声明列表
+};
+
+struct Block final : public ProgramUnit {
+    std::string id() const override {
+        return "block";
+    }
+
+    std::vector<Statement *> statementList; //!< 语句列表
+};
+
+struct FuncDef final : public ProgramUnit {
+    std::string id() const override {
+        return "func-def";
+    }
+
+    enum class Type : uint8_t {
+        Void,
+        Integer,
+        Float,
+    };
+
+    Type                    retType; //!< 返回值类型
+    std::string             ident;   //!< 函数名
+    std::vector<TypeDecl *> params;  //!< 参数列表
+    Block                  *body;    //!< 函数体
+                 //!< NOTE: 为 nullptr 时表示函数体为空
+};
+
+struct Program final : public ProgramUnit {
+    std::string id() const override {
+        return "program";
+    }
+
+    void append(auto unit)
+        requires(
+            std::is_same_v<std::remove_cvref_t<decltype(*unit)>, DeclStatement>
+            || std::is_same_v<std::remove_cvref_t<decltype(*unit)>, FuncDef>)
+    {
+        using T                   = std::remove_cvref_t<decltype(*unit)>;
+        constexpr auto isDeclUnit = std::is_same_v<T, DeclStatement>;
+        const auto     nextId     = declFlows.size() + funcFlows.size();
+        if constexpr (isDeclUnit) {
+            declFlows.push_back({nextId, unit});
+        } else {
+            funcFlows.push_back({nextId, unit});
+        }
+    }
+
+    std::vector<std::pair<size_t, DeclStatement *>> declFlows; //!< 声明语句流
+    std::vector<std::pair<size_t, FuncDef *>> funcFlows; //!< 函数定义流
+};
+
+} // namespace ssyc::ast
