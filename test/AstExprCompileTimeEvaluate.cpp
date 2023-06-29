f3d20f978b8a83b9693202002099a05d231aaaaa
周上行Ryer <melaiiryer@gmail.com>
2023-06-29 21:51:45 +0800

    test: update unit test for list and compile-time-eval

--- test/AstExprCompileTimeEvaluate.cpp
+++ test/AstExprCompileTimeEvaluate.cpp
@@ -5,7 +5,7 @@ using slime::visitor::ASTToIRVisitor;
 using namespace slime::ast;
 namespace ir = slime::ir;
 
-TEST(AstExprCompileTimeEvaluate, test_name) {
+TEST(AstExprCompileTimeEvaluate, SimpleBinaryOp) {
     ASSERT_EQ(
         3,
         static_cast<ir::ConstantInt*>(
@@ -14,5 +14,45 @@ TEST(AstExprCompileTimeEvaluate, test_name) {
                 BinaryOperator::Add,
                 ConstantExpr::createI32(1),
                 ConstantExpr::createI32(2))))
+            ->value);
+
+    ASSERT_EQ(
+        -1,
+        static_cast<ir::ConstantInt*>(
+            ASTToIRVisitor::evaluateCompileTimeAstExpr(new BinaryExpr(
+                BuiltinType::getIntType(),
+                BinaryOperator::Sub,
+                ConstantExpr::createI32(1),
+                ConstantExpr::createI32(2))))
+            ->value);
+
+    ASSERT_EQ(
+        12,
+        static_cast<ir::ConstantInt*>(
+            ASTToIRVisitor::evaluateCompileTimeAstExpr(new BinaryExpr(
+                BuiltinType::getIntType(),
+                BinaryOperator::Mul,
+                ConstantExpr::createI32(4),
+                ConstantExpr::createI32(3))))
+            ->value);
+
+    ASSERT_EQ(
+        1,
+        static_cast<ir::ConstantInt*>(
+            ASTToIRVisitor::evaluateCompileTimeAstExpr(new BinaryExpr(
+                BuiltinType::getIntType(),
+                BinaryOperator::Div,
+                ConstantExpr::createI32(4),
+                ConstantExpr::createI32(3))))
+            ->value);
+
+    ASSERT_EQ(
+        2,
+        static_cast<ir::ConstantInt*>(
+            ASTToIRVisitor::evaluateCompileTimeAstExpr(new BinaryExpr(
+                BuiltinType::getIntType(),
+                BinaryOperator::Mod,
+                ConstantExpr::createI32(5),
+                ConstantExpr::createI32(3))))
             ->value);
 }
