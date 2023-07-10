7b4a2e5f120fa2e66748b54c505a5f153282e1d2
周上行Ryer <melaiiryer@gmail.com>
2023-07-11 07:04:05 +0800

    feat: enhance CFG

--- src/slime/pass/DeadCodeElimination.cpp
+++ src/slime/pass/DeadCodeElimination.cpp
@@ -19,30 +19,19 @@ void DeadCodeEliminationPass::runOnFunction(ir::Function *target) {
         }
         isFirst = false;
 
-        if (!block->isLinear()) {
-            if (block->control()->isImmediate()) {
-                bool control =
-                    static_cast<ConstantInt *>(block->control())->value;
-                auto brInst = static_cast<BrInst *>(block->tail()->value());
-                brInst->op<1>().reset();
-                brInst->op<2>().reset();
-                if (control) {
-                    brInst->op<0>() = block->jmpIf();
-                    block->resetBranch(block->jmpIf());
-                } else {
-                    brInst->op<0>() = block->jmpElse();
-                    block->resetBranch(block->jmpElse());
-                }
+        if (block->isBranched() && block->control()->isImmediate()) {
+            auto v = static_cast<ConstantInt *>(block->control())->value;
+            if (v) {
+                block->reset(block->branch());
+            } else {
+                block->reset(block->branchElse());
             }
         }
 
-        while (block->isLinear() && block->hasBranchOut()) {
-            auto br = block->jmpIf();
-            if (br->isLinear() && block->hasBranchOut() && br->size() == 1
-                && br->tail()->value()->id() == InstructionID::Br) {
-                auto brInst     = static_cast<BrInst *>(block->tail()->value());
-                brInst->op<0>() = br->jmpIf();
-                block->resetBranch(br->jmpIf());
+        while (block->isLinear() && !block->isTerminal()) {
+            auto branch = block->branch();
+            if (branch->isLinear() && branch->size() == 1) {
+                block->reset(branch->branch());
                 continue;
             }
             break;
@@ -61,8 +50,7 @@ void DeadCodeEliminationPass::runOnFunction(ir::Function *target) {
         }
 
         if (block->size() == 0 && block->totalInBlocks() > 0) {
-            assert(target->proto()->returnType()->isVoid());
-            Instruction::createRet()->insertToTail(block);
+            block->tryMarkAsTerminal();
         }
     }
 }
