2b621e0cdf898b9e08bbfaa8a9dcdafd679c583a
周上行Ryer <melaiiryer@gmail.com>
2023-07-12 15:23:01 +0800

    feat: add ir-stage peekhole optimize pass

---
+++ src/slime/pass/Peekhole.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "pass.h"
+
+#include <slime/ir/instruction.h>
+
+namespace slime::pass {
+
+class PeekholePass : public UniversalIRPass {
+public:
+    void runOnFunction(ir::Function *target) override;
+
+protected:
+    void foldConstant(ir::Instruction *inst);
+    void foldUnaryConstant(ir::User<1> *inst);
+    void foldBinaryConstant(ir::User<2> *inst);
+    void binUserPeekholeOptmize(ir::User<2> *inst);
+};
+
+} // namespace slime::pass
